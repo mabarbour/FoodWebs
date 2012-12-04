d6792b28b4f69b35b01fad747d2ad69d1b248535
Matt Barbour <barbour@zoology.ubc.ca>
2012-12-04 21:35:30 +0100

    Andrew's section of Chapter 5

---
+++ Ch5_e_manip.md
@@ -0,0 +1,54 @@
+Chapter 5
+---------
+
+The code below illustrates how increasing the half-saturation density (Ro) of the Rozenburg-MacArthur consumer-resource model alters the stability of the system.
+
+
+```
+## Error: there is no package called 'seqinr'
+```
+
+
+
+```r
+########### set parameters and state variables for the R-M C-R model state
+########### variable values (initial values at beginning of 'experiments')
+R <- 0.6
+C <- 0.1
+i.state <- c(R = 0.6, C = 0.1)
+
+# parameter values
+r <- 1  # per capita rate of increase in resource
+K <- 2  # indicated as 1.0 in Fig 2.1 description, but I think this was a typo in the book.
+e <- 0.5  # predator conversion efficiency
+Ro <- 0.5  # half-saturation density of predator functional response
+m <- 0.5  # mortality rate of predator
+a <- 1.3  # initial attack rate of 1.3 instead of 1.2 more closely replicates the figures in the book
+p.rm1 <- c(r = r, e = e, a = a, K = K, Ro = Ro, m = m)  # create a vector for the parameters for experiment #1
+```
+
+
+The figures below are the results of three separate simulations, where the half-saturation density was decreased from 0.5 to 0.2 and all other parameters were held constant.
+
+```
+## Error: could not find function "col2alpha"
+```
+
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-31.png) 
+
+```
+## Error: could not find function "col2alpha"
+```
+
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-32.png) 
+
+```
+## Error: could not find function "col2alpha"
+```
+
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-33.png) 
+
+
+Below is a bifurcation plot, showing how the dynamics of the system changes along a continuous gradient of half-saturation densities.  Note that at high half-saturation densities, the consumers go extinct, but the stability of the system quickly changes over a short parameter range.
+![plot of chunk unnamed-chunk-4](figure/unnamed-chunk-4.png) 
+
--- FunctionalResponses.R
+++ FunctionalResponses.R
@@ -15,3 +15,35 @@ curve(a * x/x, 0, 2, xlab = "Prey Density", ylab = "Prey Killed per Predator per
 curve(w * x / (D + x)/x, 0, 2, add = TRUE, lty = 2)
 curve(w * x^2/ (D^2 + x^2)/x, 0, 2, add = TRUE, lty = 3)
 legend("bottomright", c("Type 1", "Type 2", "Type 3"), lty = 1:3, bty = "n", cex = 0.8)
+
+######### False target model for functional response.  This version was modified from Abrahamson & Weis 1997 pg. 305.  I believe their version is incorrect.  I tried using it but it does not replicate their graphs.  This current formulation does. ######
+a <- 1
+H <- 0.2 # handling time of vulnerable prey
+D <- 0 # discrimination time between vulnerable and invulnerable prey
+v <- 1.0 # fraction of vulnerable prey
+v1 <- 0.5
+v2 <- 0.25
+
+
+par(mfrow=c(1,3)) # setup graphs for easy comparison
+
+# Perfect discrimination, but decreasing the fraction of true targets
+D <- 0
+curve((a*v*x)/(1 + a*H*v*x + a*D*(1 - v)*x), from=0, to=50, ylim=c(0,5), ylab="Number encountered per parasitoid", xlab="Gall density", main="D = 0")
+curve((a*v1*x)/(1 + a*H*v1*x + a*D*(1 - v1)*x), from=0, to=50, col=2, ylim=c(0,10), add=T)
+curve((a*v2*x)/(1 + a*H*v2*x + a*D*(1 - v2)*x), from=0, to=50, col=3, add=T)
+legend("bottomright", legend=c("v = 1.0", "v = 0.5", "v = 0.25"), col=1:3, lty=1, bty="n")
+
+# weak discrimination, but decreasing the fraction of true targets
+D <- H*0.5
+curve((a*v*x)/(1 + a*H*v*x + a*D*(1 - v)*x), from=0, to=50, ylim=c(0,5), ylab="Number encountered per parasitoid", xlab="Gall density", main="D = H*0.5")
+curve((a*v1*x)/(1 + a*H*v1*x + a*D*(1 - v1)*x), from=0, to=50, col=2, ylim=c(0,10), add=T)
+curve((a*v2*x)/(1 + a*H*v2*x + a*D*(1 - v2)*x), from=0, to=50, col=3, add=T)
+legend("bottomright", legend=c("v = 1.0", "v = 0.5", "v = 0.25"), col=1:3, lty=1, bty="n")
+
+# anti-discrimination, but decreasing the fraction of true targets
+D <- H*2
+curve((a*v*x)/(1 + a*H*v*x + a*D*(1 - v)*x), from=0, to=50, ylim=c(0,5), ylab="Number encountered per parasitoid", xlab="Gall density", main="D = H*2")
+curve((a*v1*x)/(1 + a*H*v1*x + a*D*(1 - v1)*x), from=0, to=50, col=2, ylim=c(0,10), add=T)
+curve((a*v2*x)/(1 + a*H*v2*x + a*D*(1 - v2)*x), from=0, to=50, col=3, add=T)
+legend("bottomright", legend=c("v = 1.0", "v = 0.5", "v = 0.25"), col=1:3, lty=1, bty="n")
